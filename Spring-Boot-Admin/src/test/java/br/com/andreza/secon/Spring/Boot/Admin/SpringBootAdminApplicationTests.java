309a6675b1b095ad1bf3d0bff12d6e58482f9af1
Andreza Secon <andrezasecon@gmail.com>
2021-11-08 16:53:51 -0300

    Add Spring Boot Admin

---
+++ Spring-Boot-Admin/src/test/java/br/com/andreza/secon/Spring/Boot/Admin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andreza.secon.Spring.Boot.Admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
